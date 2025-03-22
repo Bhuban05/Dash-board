39a202336f7ad0d77f8194b64eefdcc68c11e516
Pemba Gelu Sherpa <pembagg122@gmail.com>
2025-03-22 11:01:57 +0545

    pemba

--- src/Components/Table/ProductTable.jsx
+++ src/Components/Table/ProductTable.jsx
@@ -1,28 +1,24 @@
 import Table from "./Table";
-const ProductTable = () => {
-  const columns = ["Product", "Price", "Stock"];
-  const data = [
-    { Product: "Laptop", Price: "$1000", Stock: "Available" },
-    { Product: "Phone", Price: "$500", Stock: "Out of Stock" },
-  ];
 
-=========
-import Datas from "../Data/Datas.json"
 import Table from "./Table";
 
- const ProductTable = () => {
+const ProductTable = () => {
+  const ProductTable = () => {
+    const columns = ["Product", "Price", "Stock"];
+    const data = [
+      { Product: "Laptop", Price: "$1000", Stock: "Available" },
+      { Product: "Phone", Price: "$500", Stock: "Out of Stock" },
+    ];
 //   // const columns = ["Product", "Price", "Stock"];
 //   // const data = [
 //   //   { Product: "Laptop", Price: "$1000", Stock: "Available" },
 //   //   { Product: "Phone", Price: "$500", Stock: "Out of Stock" },
 //   // ];
 
->>>>>>>>> Temporary merge branch 2
   return (
     
     <div>
       <h2>Product Table</h2>
-<<<<<<<<< Temporary merge branch 1
       <Table data={data} />
     </div>
   );
