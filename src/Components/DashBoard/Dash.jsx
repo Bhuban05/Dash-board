cdfdebee868c016d223d87f0b7a762d2953fd9ba
bhuban05 <bhuban.bhandari05@gmail.com>
2025-03-26 15:02:27 +0545

    new

--- src/Components/DashBoard/Dash.jsx
+++ src/Components/DashBoard/Dash.jsx
@@ -14,32 +14,26 @@ import { BoardList } from '../Education-management/Board/BoardList/BoardList'
 function Dash() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       {/* <Sidebar/> */}
-        <div className='h-screen flex ' id='container'>
+      <div className='h-screen flex ' id='container'>
 
         <div className='lg:mx-76  h-screen'>
-
-     
-          <h2 className='text-3xl  font-bold mt-25'>Board Management
+        <h2 className='text-3xl  font-bold mt-25'>Board Management
 
           </h2>
-          <p className='ms-290 flex align-item gap-3  bg-blue-700   rounded'> <IoMdAdd className='mt-1.5 space-x-5' />create a board</p>
-            </div>
-            
-     
-     
 
-            </div>
-            {/* <Crm/> */}
-           <BoardList/>
-            {/* <ProductTable/> */}
-            <StudentId/>
-           
-          
-</div>
+        </div>
+        </div>
 
-)
+     <BoardList />
+      {/* <ProductTable/> */}
+      <StudentId />
+
+      </div>
+    
+
+  )
 }
 
 export default Dash;
